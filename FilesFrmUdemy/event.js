b5d0adb12eb46321d54622774a1f8708240a5a00
Laxman Chamlagai <laxmanchamlagai20@gmail.com>
2018-12-15 00:26:24 -0500

    A simple shopping list app created with JS event listener

---
+++ FilesFrmUdemy/event.js
@@ -0,0 +1,23 @@
+var input = document.getElementById("userinput");
+var button = document.getElementById("btn");
+var ul = document.querySelector("ul");
+
+button.addEventListener("click", function() {
+    if(input.value.length > 0 ) {
+        var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+   input.value = "";
+    }
+    
+})
+
+input.addEventListener("keypress", function(event) {
+    if(input.value.length > 0 && event.keyCode === 13) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    }
+    
+})
